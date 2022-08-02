d04d7511ea8ff9629055e5033e0f9308ce712179
jacktheminecraftmodder <42350280+jacktheminecraftmodder@users.noreply.github.com>
2022-08-02 11:20:58 +0530

    added add custoemer function

--- project.py
+++ project.py
@@ -1,5 +1,8 @@
 import time
 from datetime import datetime
+from uuid import uuid4
+import uuid
+import re
 
 import numpy as np
 import pandas as pd
@@ -11,7 +14,7 @@ print(f"[{datetime.now()}] Initializing...")
 print(f"[{datetime.now()}] Loading Files...")
 
 # Read the Files
-customers = pd.read_csv('Data\customers.csv')
+customers = pd.read_csv('Data\customers.csv', index_col='id')
 orders = pd.read_csv('Data\orders.csv')
 products = pd.read_csv('Data\products.csv')
 tickets = pd.read_csv(r'Data\tickets.csv')
@@ -132,6 +135,29 @@ def amcDABarGraph():
             menu_level = "1.1.3"
             break
 
+def validate_email(email):
+    return re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', email)
+
+def date_decoder(date):
+    d, m, y = None, None, None
+    if " " in date:
+        date = date.split(" ")
+        if str(date[1]).isdigit(): return None
+        M = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        d, m, y = date[0][:2], M.index(date[1][:3]) + 1, date[2]
+    elif "/" in date:
+        date = date.split("/")
+        d, m, y = date
+    else:
+        return None
+    try:
+        datetime(year=int(y),month=int(m),day=int(d))
+        if len(y) == 2: y = "20" + y
+        return f"{int(d)}/{int(m)}/{int(y)}"
+    except ValueError as e:
+        print("Invalid date")
+        return None
+
 
 def am_cust_f():
     global menu_level
@@ -139,12 +165,105 @@ def am_cust_f():
         print_menu(menu_level)
         cmd = input("Command: ")
         if cmd == "1":
-            print("Show all Customers")
+            print(customers)
         elif cmd == "2":
             menu_level = "1.1.1"
             amc_Search()
         elif cmd == "3":
-            print("Add A Customer")
+            print("+" + "-"*25 + "ADD A CUSTOMER" + "-"*25 + "+")
+            id = input("Enter ID (Leave blank for random uuid): ")
+            while id in customers.index:
+                print("ERROR DUPLICATE ID")
+                id = input("Enter ID (Leave blank for random uuid): ")
+            if id == "": id = uuid.uuid4()
+            # first_name
+            first_name = input("Enter First Name: ")
+            while not first_name.isalpha():
+                print("First Name should only have alpha characters")
+                first_name = input("Enter First Name: ")
+            # last_name
+            last_name = input("Enter Last Name: ")
+            while not last_name.isalpha():
+                print("Last Name should only have alpha characters")
+                last_name = input("Enter Last Name: ")
+            # DOB
+            print("""Accepted Formats: 
+"16th Jan 2021"
+"16/01/2021"
+'16 January 2021'
+            """)
+            dob_check = input("Enter Dob: ")
+            dob = date_decoder(dob_check)
+            # Gender
+            gender_check = input("Enter Gender (No Abbrev): ")
+            gender = gender_check if gender_check in ["Male", "Female"] else None
+            # Address
+            address = input("Enter Address: ")
+            # Country
+            country = input("Enter Country: ")
+            while not country.isalpha():
+                print("Country should only have alpha characters")
+                country = input("Enter Country: ")
+            # City
+            city = input("Enter City: ")
+            while not city.isalpha():
+                print("City should only have alpha characters")
+                city = input("Enter City: ")
+            # State
+            state = input("Enter State: ")
+            while not state.isalpha():
+                print("State should only have alpha characters")
+                state = input("Enter State: ")
+            # Postal code
+            pincode = input("Enter Pincode: ")
+            while not pincode.isdigit():
+                print("Pincode must be a number.")
+                pincode = input("Enter Pincode: ")
+            # Phone
+            phone = input("Enter Phone: ")
+            while not phone.isdigit():
+                print("Phone must be a valid Phone.")
+                phone = input("Enter Phone: ")
+            # Email
+            email = input("Enter Email: ")
+            while not validate_email(email):
+                print("Invalid Email")
+                email = input("Enter Email: ")
+            # Prime
+            prime = "PRIME" if input("Enter Prime: ") in ["Prime", "PRIME", "Yes", "1", "Y", "P", "p"] else "-"
+            # Whole data validation
+            NewData = [first_name, last_name, dob,
+            gender, address, country, city, state, pincode,
+            phone, email, prime]
+            print("+" + "-"*50 + "+")
+            if all(NewData):
+                ll = max([len(x) for x in NewData])
+                fac = 62 if ll <= 62 else ll
+                eq = ll - 62 if ll > 62 else 0
+                print(f"""
+            +{"=" * (eq//2)}============================Please Confirm the Data Input======================={"=" * ((eq//2) + (1 if eq % 2 != 0 else 0))}+
+            | id          :    {id        }{" " * (fac - len(str(id        )))}|
+            | first_name  :    {first_name}{" " * (fac - len(str(first_name)))}|
+            | last_name   :    {last_name }{" " * (fac - len(str(last_name )))}|
+            | dob         :    {dob       }{" " * (fac - len(str(dob       )))}|
+            | gender      :    {gender    }{" " * (fac - len(str(gender    )))}|
+            | address     :    {address   }{" " * (fac - len(str(address   )))}|
+            | country     :    {country   }{" " * (fac - len(str(country   )))}|
+            | city        :    {city      }{" " * (fac - len(str(city      )))}|
+            | state       :    {state     }{" " * (fac - len(str(state     )))}|
+            | pincode     :    {pincode   }{" " * (fac - len(str(pincode   )))}|
+            | phone       :    {phone     }{" " * (fac - len(str(phone     )))}|
+            | email       :    {email     }{" " * (fac - len(str(email     )))}|
+            | prime       :    {prime     }{" " * (fac - len(str(prime     )))}|
+            +================================================================================{"=" * eq}+
+            """)
+                reck = input("Would you like to insert this data to Customers.csv ? (Y/N): ")
+                if reck.lower() == "y": 
+                    customers.loc[id] = NewData
+                else:
+                    print("Record Insertion Cancelled.")
+            else:
+                print(f"Error: Invalid Data: {NewData}")
         elif cmd == "4":
             print("Update A Customer")
         elif cmd == "5":
